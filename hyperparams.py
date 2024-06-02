e73e9d3b68edbb2d7fcbd5312897fa5ed473e290
aimas <cata_chiru@yahoo.com>
2024-06-02 14:12:32 +0300

    CC2:Init git project with LungCancer

---
+++ hyperparams.py
@@ -0,0 +1,18 @@
+### Hyperparameters for the models ###
+
+
+BATCH_SIZE = 4
+
+IMG_HEIGHT = 128
+IMG_WIDTH = 128
+COLOUR_CHANNELS = 1
+NO_STACKED_IMGS = 64 # FOLOSESC DIRECT MINIBATCH-URI DE CATE 6 IMAGINI
+
+KEYS = ["image", "label"]
+
+LEARNING_RATE = 3e-3
+decayRate = 0.96
+
+
+MAX_EPOCHS = 10
+VALIDATION_INTERVAL = 2
